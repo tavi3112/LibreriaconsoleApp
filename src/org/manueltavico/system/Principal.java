fb6005727f6adb6c57ed3183bf7e9173d20a1747
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-06-04 11:31:01 -0600

    000 - primer commit configuracion basica y .gitignore

---
+++ src/org/manueltavico/system/Principal.java
@@ -0,0 +1,15 @@
+
+package org.manueltavico.system;
+
+/**
+ *
+ * @author informatica
+ */
+public class Principal {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
