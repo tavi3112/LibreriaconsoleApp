cf57ad8a7a61c9dcaecd591628608086fe83f72d
manuel tavico <mtavico-2026272@kinal.edu.gt>
2026-06-05 10:44:33 -0600

    008 se agrego hola mundo

--- src/org/manueltavico/system/Principal.java
+++ src/org/manueltavico/system/Principal.java
@@ -9,7 +9,8 @@ public class Principal {
 
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo - soy manuel ");
+        System.out.println("Estoy en kinal mi carnet - 2026272");
     }
     
 }
