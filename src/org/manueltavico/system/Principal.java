3a6960f98d1967b154065799aa0e1a50125ae1bf
JoaquinSanchez <jsanchez-2026117@kinal.edu.gt>
2026-06-05 10:45:27 -0600

    010 Se actualizo la clase principal, agregue un saludo

--- src/org/manueltavico/system/Principal.java
+++ src/org/manueltavico/system/Principal.java
@@ -9,7 +9,8 @@ public class Principal {
 
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo - Soy Joaquin Sanchez");
+        System.out.println("Estoy en kinal - 2026117");
     }
     
 }
