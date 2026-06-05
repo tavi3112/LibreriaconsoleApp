f042c52bee5faeeb14bbe44d3149bd6d566f3a92
AndresAquino <aaquino-2023534@kinal.edu.gt>
2026-06-05 10:46:43 -0600

    011 Se actualizo clase principal, agregue un saludo

--- src/org/manueltavico/system/Principal.java
+++ src/org/manueltavico/system/Principal.java
@@ -9,7 +9,8 @@ public class Principal {
 
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo - soy Andres Aquino");
+        System.out.println("Estoy en kinal - 2023534");
     }
     
 }
