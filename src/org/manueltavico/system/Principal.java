25204bf849aa209a12e5d8dcef441d718852c9ca
Informatica <informatica@alumno.local>
2026-06-05 10:45:34 -0600

    009 se actualizo la clase Principal, agregue un saludo

--- src/org/manueltavico/system/Principal.java
+++ src/org/manueltavico/system/Principal.java
@@ -9,7 +9,8 @@ public class Principal {
 
     
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo - Soy Kevin");
+        System.out.println("Estoy en kinal - 2026320");
     }
     
 }
